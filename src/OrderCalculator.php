14d917748787a1444e00a901e5989777576bda0d
Cedric Lesquir <clesquir@devalto.com>
2016-06-03 11:26:25 -0400

    Created const

--- src/OrderCalculator.php
+++ src/OrderCalculator.php
@@ -4,6 +4,18 @@ namespace Star\Training;
 
 class OrderCalculator {
 
+	const ACCOUNT_TYPE_BRONZE = 1;
+	const ACCOUNT_TYPE_SILVER = 2;
+	const ACCOUNT_TYPE_GOLD = 3;
+	const ITEM_RARITY_UNCOMMON = 2;
+	const ITEM_RARITY_RARE = 3;
+	const ACCOUNT_PRICE_REBATE_BRONZE = 0.90;
+	const ACCOUNT_PRICE_REBATE_SILVER = 0.80;
+	const ACCOUNT_PRICE_REBATE_GOLD = 0.70;
+	const ITEM_PRICE_BOOST_UNCOMMON = 1.5;
+	const ITEM_PRICE_BOOST_RARE = 2;
+
+
 	function calculate($order, $customer = null) {
 		$sum = 0;
 		$accountType = (isset($customer['account_level'])) ? $customer['account_level'] : 'Normal';
@@ -23,25 +35,25 @@ class OrderCalculator {
 
 			$itemQuantity[$item['type']] ++;
 
-			if (1 === $accountType) {
-				$price *= .90;
+			if (self::ACCOUNT_TYPE_BRONZE === $accountType) {
+				$price *= self::ACCOUNT_PRICE_REBATE_BRONZE;
 			}
 
-			if (2 === $item['type']) {
-				$price *= 1.5;
+			if (self::ITEM_RARITY_UNCOMMON === $item['type']) {
+				$price *= self::ITEM_PRICE_BOOST_UNCOMMON;
 			}
 
-			if (3 === $item['type']) {
-				$price *= 2;
+			if (self::ITEM_RARITY_RARE === $item['type']) {
+				$price *= self::ITEM_PRICE_BOOST_RARE;
 			}
 
-			if (2 === $accountType) {
-				$price *= .80;
+			if (self::ACCOUNT_TYPE_SILVER === $accountType) {
+				$price *= self::ACCOUNT_PRICE_REBATE_SILVER;
 			}
 
-			if (3 === $accountType) {
+			if (self::ACCOUNT_TYPE_GOLD === $accountType) {
 				// 2 for one
-				if (2 === $itemQuantity[$item['type']] && 3 === $item['type']) {
+				if (self::ITEM_RARITY_UNCOMMON === $itemQuantity[$item['type']] && self::ITEM_RARITY_RARE === $item['type']) {
 					$itemQuantity[$item['type']] = 0;
 					$price = $highestPrice[$item['type']] - $price;
 					if ($price < 0) {
@@ -53,8 +65,8 @@ class OrderCalculator {
 			$sum += $price;
 		}
 
-		if (3 === $accountType) {
-			$sum *= .70;
+		if (self::ACCOUNT_TYPE_GOLD === $accountType) {
+			$sum *= self::ACCOUNT_PRICE_REBATE_GOLD;
 		}
 
 		return number_format($sum, 2);
