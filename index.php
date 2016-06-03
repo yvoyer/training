46199e68984510901b2cfc6f79cf51ba8df85123
Vincent Guillemette <vguillemette@devalto.com>
2016-06-03 13:28:11 -0400

    all-in

--- index.php
+++ index.php
@@ -1,66 +1,164 @@
 <?php
 // ... other code
 
+class Order {
+    private $_items;
+    
+    private function __construct($items) {
+        $this->_items = items;
+    }
+    
+    public static function fromItems($items) {
+        return new self($items);
+    }
+    
+    public function getItems() {
+        return $this->_items;
+    }
+}
+
+class OrderItem {
+    const COMMON = 1;
+    const UNCOMMON = 2;
+    const RARE = 3;
+    
+    private $_type;
+    private $_price;
+
+    private function __construct($type, $price) {
+        $this->_type = $type;
+        $this->_price = $price;
+    }
+    
+    public static function fromDefault($price) {
+        return new self(OrderItem::COMMON, $price);
+    }
+    
+    public static function fromTypeAndPrice($type, $price) {
+        return new self($type, $price);
+    }
+    
+    public function getType() {
+        return $this->_type;
+    }
+
+    public function getPrice() {
+        return $this->_price;
+    }
+}
+
+class Customer {
+    const BRONZE_ACCOUNT = 1;
+    const SILVER_ACCOUNT = 2;
+    const GOLD_ACCOUNT = 3;
+
+    private $_accountLevel;
+
+    private function __construct($accountLevel){
+        $this->_accountLevel = $accountLevel;
+    }
+
+    public static function fromDefault() {
+        return new self(Customer::SILVER_ACCOUNT);
+    }
+
+    public static function fromAccountLevel($accountLevel) {
+        return new self($accountLevel);
+    }
+
+    public function getAccountLevel() {
+        return $this->_accountLevel;
+    }
+}
+
+class Calculator {
+
+    public function __construct() {}
+
+    public function calculateOrder(Order $order, Customer $customer = null) {
+        $orderItems = $order->getItems();
+        $sum = 0;
+
+        $itemQuantity[1] = 0;
+        $itemQuantity[2] = 0;
+        $itemQuantity[3] = 0;
+        $highestPrice[1] = 0;
+        $highestPrice[2] = 0;
+        $highestPrice[3] = 0;
+
+        foreach ($orderItems as $item) {
+            $itemPrice = $item.getPrice();
+            $itemType = $item.getType();
+            $accountLevel = $customer->getAccountLevel();
+
+            if ($itemPrice > $highestPrice[$item.getType()]) {
+                $highestPrice[$itemType] = $itemPrice;
+            }
+
+            $itemQuantity[$itemType] ++;
+
+            switch ($accountLevel) {
+                case Customer::BRONZE_ACCOUNT:
+                    $itemPrice *= .90;
+                    break;
+                case Customer::SILVER_ACCOUNT:
+                    $itemPrice *= .80;
+                    break;
+                case Customer::GOLD_ACCOUNT:
+                    $itemPrice *= .70;
+                    break;
+            }
+
+            if (2 === $itemType) {
+                $itemPrice *= 1.5;
+            }
+
+            if (3 === $itemType) {
+                $itemPrice *= 2;
+            }
+
+            if (Customer::GOLD_ACCOUNT == $accountLevel) {
+                // 2 for one
+                if (2 === $itemQuantity[$itemType] && 3 === $itemType) {
+                    $itemQuantity[$itemType] = 0;
+                    $itemPrice = $highestPrice[$itemType] - $itemPrice;
+                    if ($itemPrice < 0) {
+                        $itemPrice = 0;
+                    }
+                }
+            }
+
+            $sum += $itemPrice;
+        }
+
+        return number_format($sum, 2);
+    }
+
+}
+
 /**
  * @param object $order
  * @param array $customer
  *
  * @return string The total price to pay for the $customer
  */
-function calculateOrder($order, $customer = null) {
-    $sum = 0;
-    $accountType = (isset($customer['account_level'])) ? $customer['account_level'] : 'Normal';
+function calculateOrder($orderData, $customerData = null) {
+    $orderItems = array();
 
-    $itemQuantity[1] = 0;
-    $itemQuantity[2] = 0;
-    $itemQuantity[3] = 0;
-    $highestPrice[1] = 0;
-    $highestPrice[2] = 0;
-    $highestPrice[3] = 0;
-
-    foreach ($order->items as $item) {
-        $price = $item['price'];
-        if ($price > $highestPrice[$item['type']]) {
-            $highestPrice[$item['type']] = $price;
-        }
-
-        $itemQuantity[$item['type']] ++;
-
-        if (1 === $accountType) {
-            $price *= .90;
-        }
-
-        if (2 === $item['type']) {
-            $price *= 1.5;
-        }
-
-        if (3 === $item['type']) {
-            $price *= 2;
-        }
-
-        if (2 === $accountType) {
-            $price *= .80;
-        }
-
-        if (3 === $accountType) {
-            // 2 for one
-            if (2 === $itemQuantity[$item['type']] && 3 === $item['type']) {
-                $itemQuantity[$item['type']] = 0;
-                $price = $highestPrice[$item['type']] - $price;
-                if ($price < 0) {
-                    $price = 0;
-                }
-            }
-        }
-
-        $sum += $price;
+    foreach ($orderData->items as $item){
+        array_push($orderItems, OrderItem::fromTypeAndPrice($item['type'], $item['price']));
     }
 
-    if (3 === $accountType) {
-        $sum *= .70;
+    $order = Order::fromItems($orderItems);
+
+    if (!$customerData) {
+        $customer = Customer::fromDefault();
+    } else {
+        $customer = Customer::fromAccountLevel($customerData['account_level']);
     }
 
-    return number_format($sum, 2);
+    $calculator = new Calculator();
+    return  $calculator->calculateOrder($order, $customer);
 }
 
 // ... other code
