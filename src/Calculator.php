6100ff95b3bb4dd0d899dda8d84122eb17636fdf
Christian Fecteau <christian.fecteau@gmail.com>
2016-06-03 11:25:59 -0400

    extract types to constants

--- src/Calculator.php
+++ src/Calculator.php
@@ -4,35 +4,42 @@ namespace Star\Training;
 
 class Calculator {
 
+	const ITEM_TYPE_1 = 1;
+
+	const ITEM_TYPE_2 = 2;
+
+	const ITEM_TYPE_3 = 3;
+
 	public function calculate($order, $customer = null) {
 
 		$sum = 0;
 		$accountType = (isset($customer['account_level'])) ? $customer['account_level'] : 'Normal';
 
-		$itemQuantity[1] = 0;
-		$itemQuantity[2] = 0;
-		$itemQuantity[3] = 0;
-		$highestPrice[1] = 0;
-		$highestPrice[2] = 0;
-		$highestPrice[3] = 0;
+		$itemQuantity[self::ITEM_TYPE_1] = 0;
+		$itemQuantity[self::ITEM_TYPE_2] = 0;
+		$itemQuantity[self::ITEM_TYPE_3] = 0;
+		$highestPrice[self::ITEM_TYPE_1] = 0;
+		$highestPrice[self::ITEM_TYPE_2] = 0;
+		$highestPrice[self::ITEM_TYPE_3] = 0;
 
 		foreach ($order->items as $item) {
 			$price = $item['price'];
-			if ($price > $highestPrice[$item['type']]) {
-				$highestPrice[$item['type']] = $price;
+			$type = $item['type'];
+			if ($price > $highestPrice[$type]) {
+				$highestPrice[$type] = $price;
 			}
 
-			$itemQuantity[$item['type']]++;
+			$itemQuantity[$type]++;
 
 			if (1 === $accountType) {
 				$price *= .90;
 			}
 
-			if (2 === $item['type']) {
+			if (self::ITEM_TYPE_2 === $type) {
 				$price *= 1.5;
 			}
 
-			if (3 === $item['type']) {
+			if (self::ITEM_TYPE_3 === $type) {
 				$price *= 2;
 			}
 
@@ -42,9 +49,9 @@ class Calculator {
 
 			if (3 === $accountType) {
 				// 2 for one
-				if (2 === $itemQuantity[$item['type']] && 3 === $item['type']) {
-					$itemQuantity[$item['type']] = 0;
-					$price = $highestPrice[$item['type']] - $price;
+				if (self::ITEM_TYPE_2 === $itemQuantity[$type] && self::ITEM_TYPE_3 === $type) {
+					$itemQuantity[$type] = 0;
+					$price = $highestPrice[$type] - $price;
 					if ($price < 0) {
 						$price = 0;
 					}
